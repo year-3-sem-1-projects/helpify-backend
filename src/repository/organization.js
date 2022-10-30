e5b1d45977423fc7738f7af612c2cd83f76ef0e1
christancf <christanf6@gmail.com>
2022-10-30 21:04:50 +0530

    Add event functionality added

--- src/repository/organization.js
+++ src/repository/organization.js
@@ -1,8 +1,27 @@
 import Organization from '../models/organization'
+import Event from '../models/event'
+import Story from '../models/story'
 
 export const addOrganizationRepository = async (data) => {
   try {
     const result = await new Organization(data).save()
+    return result
+  } catch (error) {
+    return { status: error.status, message: error.message }
+  }
+}
+
+export const addEventRepository = async (organizationId, data) => {
+  console.log('organizationId:::::::::;', organizationId)
+  console.log('data:::::::', data)
+  try {
+    const { _id } = await new Event(data).save()
+
+    const result = await Organization.findByIdAndUpdate(
+      organizationId,
+      { $push: { events: _id } },
+      { new: true },
+    )
     return result
   } catch (error) {
     return { status: error.status, message: error.message }
